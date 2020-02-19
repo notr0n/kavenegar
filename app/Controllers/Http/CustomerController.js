8eb6b1a69f53f16bde1a18a7022577a1d2de1c8d
shayan <iamsha4yan@gmail.com>
2020-02-19 20:32:47 +0330

    v2

--- app/Controllers/Http/CustomerController.js
+++ app/Controllers/Http/CustomerController.js
@@ -2,7 +2,6 @@
 var Kavenegar = require('kavenegar');
 
 var ApiKey = '674D444A44786D5574735A3851326D6744685330472F4578384530786C362F4F676850635A6A4B4D496E633D';
-var Customer = "09167212043";
 var Sender = "1000596446";
 var code = Math.floor(Math.random() * 10000) + 12345;
 console.log(code);
@@ -10,6 +9,7 @@ var Message = "کد اپلیکیشن موتور " + code;
 
 class CustomerController {
   async create ({ request, response}) {
+      var Customer = request.param('phone');
       response.send(Message)
       var api = Kavenegar.KavenegarApi({
         apikey: ApiKey
